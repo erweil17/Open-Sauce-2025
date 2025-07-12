214a52614c95d5062ad2be4b2ea720e8f49daa27
erweil17 <eric.weiler.10@gmail.com>
2025-07-11 21:10:29 -0700

    Motor receiver working

---
+++ Arduino/OS2025_Motor_Receiver/OS2025_Motor_Receiver.ino
@@ -0,0 +1,76 @@
+
+#include <Arduino.h>
+#include <RotaryEncoder.h>
+
+
+#define PIN_IN1 2
+#define PIN_IN2 3
+
+uint8_t fifoByte = 0;
+uint8_t bitCount = 0;
+bool halfturn_right = 0;
+bool halfturn_left = 0;
+
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
+
+void setup()
+{
+  Serial.begin(9600);
+} 
+
+
+// Read the current position of the encoder and saves it as a bit into a one bite array. first in first out. once the byte is full (8 position changes),
+// prints out the value in base ten. Then resets.
+void loop()
+{
+  static int pos = 0;
+  
+  encoder.tick();
+
+  int newPos = encoder.getPosition();
+  int bit = (int)(encoder.getDirection());
+
+  
+  if (pos != newPos) {
+    pos = newPos;
+    
+    //if turned right
+    if (bit == 1){
+      //if we already turned half way
+      if (halfturn_right == 1){
+        shiftInBit(1);
+        halfturn_left = 0;
+        halfturn_right = 0;
+      }
+      //this is only half way
+      else{
+        halfturn_right = 1;
+        halfturn_left = 0;
+      }
+    }
+
+    //if turned left
+    if (bit == -1){
+      if (halfturn_left == 1){
+        shiftInBit(0);
+        halfturn_right = 0;
+        halfturn_left = 0;
+      }
+      else{
+        halfturn_left = 1;
+        halfturn_right = 0;
+      }
+    }
+  } 
+} 
+
+void shiftInBit(uint8_t bit) {
+  bit = bit & 0x01;                 // Ensure it's 0 or 1
+  fifoByte = (fifoByte << 1) | bit; // Shift left and add bit
+  bitCount++;
+  if (bitCount == 8) {
+    Serial.write(char(fifoByte));
+    bitCount = 0;
+    fifoByte = 0; // Reset if needed
+  }
+}
